9cff81ecb22ff4e1701d42c29537de2bb559686b
yuri6 <yuri6@210.95.207.150>
2022-12-01 20:44:06 +0900

    array 연습문제

---
+++ a_java/src/com/mc/b_operator/Quize.java
@@ -0,0 +1,37 @@
+package com.mc.b_operator;
+
+import java.util.Scanner;
+
+public class Quize {
+
+	public static void main(String[] args) {
+		q1();
+		q2();
+		q3();
+	}
+
+	private static void q3() {
+		//q3. 사용자가 입력한 정수가 짝수 인지 판단하고
+		//짝수라면 '짝수 입니다.'
+		//홀수라면 '홀수 입니다.' 를 출력하세요. 
+		Scanner sc = new Scanner(System.in);
+	}
+
+	
+	private static void q2() {
+		//q2. 사용자로 부터 아이디와 비밀번호를 입력받아
+		// 사용자가 알맞은 아이디와 비밀번호를 입력하였다면, '로그인 되었습니다'를 출력하고
+		// 아이디와 비밀번호가 맞지 않다면 '누구야 당신?' 을 출력하세요
+		// 아이디 : test / 비밀번호 : java
+		
+		//사용자로부터 아이디와 비밀번호를 입력받는다.
+		Scanner sc = new Scanner(System.in);	
+	}
+
+	private static void q1() {
+		// q1. 사용자로 부터 사다리꼴의 윗변, 아랫변, 높이를 입력받아 넓이를 구하시오 
+		//사용자로 부터 정수를 하나 입력 받으세요
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
