3d98fab579b24abdf766e5e36daac8b5c7bef001
yuri6 <yuri6@210.95.61.192>
2022-12-02 22:37:38 +0900

    poly

---
+++ b_oop_Git/src/com/mc/e_poly/SmartPhone.java
@@ -0,0 +1,35 @@
+package com.mc.e_poly;
+
+public class SmartPhone extends Computer{
+
+	private String mobileAgency;
+
+	public SmartPhone() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SmartPhone(String brand, String pName, int price, String cpu, int ram) {
+		super(brand, pName, price, cpu, ram);
+		this.mobileAgency = mobileAgency;
+	}
+
+	public String getMobileAgency() {
+		return mobileAgency;
+	}
+
+	public void setMobileAgency(String mobileAgency) {
+		this.mobileAgency = mobileAgency;
+	}
+
+	public String toString() {
+		return"SmartPhone [brand=" + brand + ", pName=" + pName + ", price=" + price + ", cpu=" + cpu + ", ram=" + ram
+	            + ", mobileAgency=" + mobileAgency + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
