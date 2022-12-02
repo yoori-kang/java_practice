de2018308340ed3e9da08a8deab2a02db40d9241
yuri6 <yuri6@210.95.61.192>
2022-12-02 22:34:50 +0900

    access modifier test

---
+++ b_oop_Git/src/com/mc/z_test/AccessModifierTest.java
@@ -0,0 +1,13 @@
+package com.mc.z_test;
+
+import com.mc.a_modifier.A_accessModifier;
+
+public class AccessModifierTest {
+
+	public static void main(String[] args) {
+		
+		A_accessModifier.publicMethod();
+		
+	}
+
+}
