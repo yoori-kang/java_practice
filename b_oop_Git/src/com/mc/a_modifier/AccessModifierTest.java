291715408f12e0a9d71e34281a6ea6d524c246fa
yuri6 <yuri6@210.95.61.192>
2022-12-02 22:34:21 +0900

    modifier

---
+++ b_oop_Git/src/com/mc/a_modifier/AccessModifierTest.java
@@ -0,0 +1,15 @@
+package com.mc.a_modifier;
+
+import com.mc.a_modifier.A_accessModifier;
+
+public class AccessModifierTest {
+
+	public static void main(String[] args) {
+		
+		A_accessModifier.publicMethod();
+		A_accessModifier.defaultMethod();
+		A_accessModifier.protectedMethod();
+		//A_accessModifier.privateMethod();
+	}
+
+}
