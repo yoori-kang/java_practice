651e8c9cc5d14f3384bf4ef6408fff13db4f8177
yuri6 <yuri6@222.120.96.5>
2022-12-08 09:59:17 +0900

    StringBuilder, StringBuffer

--- d_library_Git/src/com/mc/lang/string/StringConcatenation.java
+++ d_library_Git/src/com/mc/lang/string/StringConcatenation.java
@@ -1,5 +1,35 @@
 package com.mc.lang.string;
 
-public class StringConcatenation {
+import javax.print.DocFlavor.STRING;
 
+//String은 생성된 이후 값을 변경할 수 없는 불변(immutable)객체이다.
+//String a = "a"; a+="B"; "aB"
+//String을 결합하여 수정할 경우, 해당 인스턴스 값이 변경되는 것이 아니라 새로운 인스턴스를 생성한다.
+
+public class StringConcatenation {
+	
+	public void testConcat() {
+		
+		//String constant pool 에 "JAVA" 라는 값을 가지는 객체 생성
+		String java = "JAVA";
+		String java2 = "JAVA";
+		System.out.println("수정 전 주소 : " + System.identityHashCode(java));
+		System.out.println("리터럴 JAVA 주소 : " + System.identityHashCode("JAVA"));
+		System.out.println("java2 주소 : " +System.identityHashCode(java2));
+		
+		//같은 값의 문자열을 리터럴로 생성할 경우 등위비교연산자를 사용하면 true가 나온다.
+		//리터럴로 생성할 경우 같은 값이면, 같은 객체이기 때문!
+		System.out.println("java == java2 :" +java ==java2);
+		
+		//생성자를 사용해 String을 생성할 경우, String Constant pool 대신 일반적인 heap영역에 생성된다.
+		//String constant pool을 사용해 얻을 수 있는 메모리상 이점이 사라지게 된다.
+		String java3 = new String("JAVA");
+		System.out.println("java == java3 :" + java == java3);
+		
+		
+		java += "Script";
+		System.out.println("수정 후 주소 : " + System.identityHashCode(java));
+	}
+	
+	
 }
